02382206f9d8e117e49cb721ca305055b01dfecb
Alexander Kuzmenko <alex@stablex.ru>
2015-09-24 15:27:00 +0300

    tests for RendererHolder.padding

--- src/sx/widgets/RendererHolderTest.hx
+++ src/sx/widgets/RendererHolderTest.hx
@@ -19,18 +19,21 @@ class RendererHolderTest extends TestCase
     {
         var renderer = mock(Renderer).create();
         var rendererResized = null;
+        stub(renderer).getWidth().returns(100);
+        stub(renderer).getHeight().returns(200);
         stub(renderer).onResize().implement(function (callback) rendererResized = callback);
 
         var holder = new TestingRendererHolder(renderer);
+        holder.padding.px = 10;
         holder.autoSize.set(true);
 
         //simulate callback invokation by renderer
-        rendererResized(100, 200);
+        rendererResized(renderer.getWidth(), renderer.getHeight());
 
         assert.equal(Pixel, holder.width.units);
         assert.equal(Pixel, holder.height.units);
-        assert.equal(100., holder.width.px);
-        assert.equal(200., holder.height.px);
+        assert.equal(120., holder.width.px);
+        assert.equal(220., holder.height.px);
     }
 
 
@@ -118,6 +121,19 @@ class RendererHolderTest extends TestCase
 
 
     @test
+    public function width_setWidthDirectly_invokesRendererSetAvailableAreaWidth () : Void
+    {
+        var renderer = mock(Renderer).create();
+        var holder = new TestingRendererHolder(renderer);
+        holder.padding.horizontal.px = 10;
+
+        expect(renderer).setAvailableAreaWidth(80.).once();
+
+        holder.width.px = 100;
+    }
+
+
+    @test
     public function height_setHeightDirectly_changesAutoSizeHeightToFalse () : Void
     {
         var holder = new TestingRendererHolder(mock(Renderer).create());
@@ -126,6 +142,105 @@ class RendererHolderTest extends TestCase
         holder.height.px = 100;
 
         assert.isFalse(holder.autoSize.height);
+    }
+
+
+    @test
+    public function height_setHeightDirectly_invokesRendererSetAvailableAreaHeight () : Void
+    {
+        var renderer = mock(Renderer).create();
+        var holder = new TestingRendererHolder(renderer);
+        holder.padding.vertical.px = 10;
+
+        expect(renderer).setAvailableAreaHeight(80.).once();
+
+        holder.height.px = 100;
+    }
+
+
+    @test
+    public function paddingHorizontal_changedWhileAutoSizeWidthIsFalse_invokesRendererSetAvailableAreaWidth () : Void
+    {
+        var renderer = mock(Renderer).create();
+        var holder = new TestingRendererHolder(renderer);
+        holder.autoSize.width = false;
+        holder.width.px = 100;
+
+        expect(renderer).setAvailableAreaWidth(80.).once();
+
+        holder.padding.horizontal.px = 10;
+    }
+
+
+    @test
+    public function paddingVertical_changedWhileAutoSizeHeightIsFalse_invokesRendererSetAvailableAreaHeight () : Void
+    {
+        var renderer = mock(Renderer).create();
+        var holder = new TestingRendererHolder(renderer);
+        holder.autoSize.height = false;
+        holder.height.px = 100;
+
+        expect(renderer).setAvailableAreaHeight(80.).once();
+
+        holder.padding.vertical.px = 10;
+    }
+
+
+    @test
+    public function paddingHorizontal_changedWhileAutoSizeWidthIsTrue_dispatchesOnResizeSignalWithHolderWidth () : Void
+    {
+        var dispatched = 0;
+        var renderer = mock(Renderer).create();
+        stub(renderer).getWidth().returns(100);
+        var holder = new TestingRendererHolder(renderer);
+        holder.autoSize.width = true;
+        holder.onResize.invoke(function(w, s, u, v) {
+            if (s == holder.width) dispatched ++;
+        });
+
+        holder.padding.horizontal.px = 10;
+
+        assert.equal(1, dispatched);
+        assert.equal(120., holder.width.px);
+    }
+
+
+    @test
+    public function paddingVertical_changedWhileAutoSizeHeightIsTrue_dispatchesOnResizeSignalWithHolderHeight () : Void
+    {
+        var dispatched = 0;
+        var renderer = mock(Renderer).create();
+        stub(renderer).getHeight().returns(100);
+        var holder = new TestingRendererHolder(renderer);
+        holder.autoSize.height = true;
+        holder.onResize.invoke(function(w, s, u, v) {
+            if (s == holder.height) dispatched ++;
+        });
+
+        holder.padding.vertical.px = 10;
+
+        assert.equal(1, dispatched);
+        assert.equal(120., holder.height.px);
+    }
+
+
+    @test
+    public function padding_setToPercentWhileAutoSizeIsTrue_holderSizeAndPaddingCalculatedCorrectly () : Void
+    {
+        var renderer = mock(Renderer).create();
+        stub(renderer).getWidth().returns(800);
+        stub(renderer).getHeight().returns(40);
+        var holder = new TestingRendererHolder(renderer);
+        holder.autoSize.set(true);
+
+        holder.padding.pct = 10;
+
+        assert.equal(1000., holder.width.px);
+        assert.equal(50., holder.height.px);
+        assert.equal(100., holder.padding.left.px);
+        assert.equal(100., holder.padding.right.px);
+        assert.equal(5., holder.padding.top.px);
+        assert.equal(5., holder.padding.bottom.px);
     }
 
 }//class RendererHolderTest
