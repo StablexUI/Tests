091f6d9bdd8fdc4b99748c13f90ed5aa1add98a7
Alexander Kuzmenko <alex@stablex.ru>
2015-10-28 15:28:56 +0300

    AAlign test

---
+++ src/sx/properties/abstracts/AAlignTest.hx
@@ -0,0 +1,26 @@
+package sx.properties.abstracts;
+
+import hunit.TestCase;
+import sx.properties.abstracts.AAlign;
+import sx.properties.Align;
+
+
+
+/**
+ * sx.properties.abstracts.AAlign
+ *
+ */
+class AAlignTest extends TestCase
+{
+
+    @test
+    public function fromNone_always_bothVerticalHorizontalBecomesNone () : Void
+    {
+        var align : AAlign = None;
+
+        assert.equal(VNone, align.vertical);
+        assert.equal(HNone, align.horizontal);
+    }
+
+
+}//class AAlignTest
