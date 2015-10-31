0474d349622ba53c0726552399465327a5541e95
Alexander Kuzmenko <alex@stablex.ru>
2015-10-31 19:20:13 +0300

    more LinkedList tests

--- src/sx/ds/LinkedListTest.hx
+++ src/sx/ds/LinkedListTest.hx
@@ -26,18 +26,6 @@ class LinkedListTest extends TestCase
 
 
     @test
-    public function push_hadItemsButNowEmpty_pushedItemBecomesFirstItem () : Void
-    {
-        var list = new TestList();
-        var item = 'hello';
-
-        list.push(item);
-
-        assert.equal(item, list.first);
-    }
-
-
-    @test
     public function push_noItemsInList_pushedItemBecomesLastItem () : Void
     {
         var list = new TestList();
@@ -123,5 +111,36 @@ class LinkedListTest extends TestCase
 
         assert.equal(item, list.first);
     }
+
+
+    @test
+    public function remove_lastItem_previouslyPushedBecomesLast () : Void
+    {
+        var list = new TestList();
+        list.push('hello');
+        list.push('world');
+
+        var removed = list.remove('world');
+
+        assert.isTrue(removed);
+        assert.equal('hello', list.last);
+    }
+
+
+    @test
+    public function remove_firstItem_secondItemBecomesFirst () : Void
+    {
+        var list = new TestList();
+        list.push('hello');
+        list.push('world');
+
+        var removed = list.remove('hello');
+
+        assert.isTrue(removed);
+        assert.equal('world', list.first);
+    }
+
+
+
 
 }//class LinkedListTest
